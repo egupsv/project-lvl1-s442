4053cb01f4e03974d976fbeae1bfcbb33a53d791
egupsv <egupsv@mail.ru>
2019-02-04 23:05:33 +0400

    fixed brain-games.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import readlineSync  from 'readline-sync';
 export const greeting = () => {
-    const userName = readlineSync.question('May I have your name?');
+    const userName = readlineSync.question('May I have your name? ');
     return (`Hello, ${userName}!`);
 }
