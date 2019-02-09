01d75bed6a607166b5d0c6c484ec37d512bbe089
egupsv <egupsv@mail.ru>
2019-02-09 13:42:54 +0400

    fix progression

--- src/games/progression.js
+++ src/games/progression.js
@@ -4,22 +4,25 @@ import getRandomInt from '../utils';
 const startMessage = 'What number is missing in the progression?';
 const progressionLength = 10;
 const maxDelta = 10;
+const getArrayForQuestion = (firstNum, delta) => {
+  const iter = (acc, counter) => {
+    if (counter === progressionLength) {
+      return acc;
+    }
+    return iter([...acc, counter * delta + firstNum], counter + 1);
+  };
+  return iter([], 0);
+};
+
 const gameStep = () => {
   const firstNum = getRandomInt();
   const delta = getRandomInt(1, maxDelta);
   const positionOfMissingElement = getRandomInt(0, progressionLength - 1);
-  const getArrayForQuestion = (acc, counter) => {
-    if (counter === progressionLength) {
-      return acc;
-    }
-    return getArrayForQuestion([...acc, counter * delta + firstNum], counter + 1);
-  };
-  const question = getArrayForQuestion([], 0)
+  const question = getArrayForQuestion(firstNum, delta)
     .map((e, i) => (i === positionOfMissingElement ? '..' : e))
     .join(' ');
-  const getRightAnswer = String(getArrayForQuestion([], 0)[positionOfMissingElement]);
+  const getRightAnswer = String(positionOfMissingElement * delta + firstNum);
   return [question, getRightAnswer];
 };
-const progression = () => game(startMessage, gameStep);
 
-export default progression;
+export default () => game(startMessage, gameStep);
